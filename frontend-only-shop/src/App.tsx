dd5ab3d8ef36158ea0027e8b888baaa4ac818706
JohnDurand33 <142426904+JohnDurand33@users.noreply.github.com>
2024-05-01 12:33:06 -0600

    about to try defining ProductType differently

--- frontend-only-shop/src/App.tsx
+++ frontend-only-shop/src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useEffect, useState } from 'react'
 import Product from './Product'
 import {ProductProps} from './Product'
 import './App.css'
@@ -6,17 +7,38 @@ import './App.css'
 type ProductArray = ProductProps['product'][]
 
 function App() {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [products, ] = useState<ProductArray>([])
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const showProducts = async () => {
+    const [products, getproducts] = useState<ProductArray>([])
+    
+    const getProducts = async () => {
+        const url = 'https://api.store.com/v1/products'
+        const options = {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${process.env.STRIPE_API_KEY}`
+            }
+        };
+
+        const res = await fetch(url, options);
+        const data = await res.json();
+        console.log(data)
+        console.log(res.status)
+        if (res.status === 200) {
+            getproducts(data)
+        }
+        
+    }
+    
+    useEffect(()=>{ getProducts() }, [])
+
+    const showProducts =() => {
         return products.map((p) => <Product key={p.id} product={p} />)
     }
 
     return (
         <div>
-
+            <h1>My Shop</h1>
+            {showProducts()}
         </div>
     )
 }
